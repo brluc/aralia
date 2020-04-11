64c393ec43d166497afa3ff5bb4c045b0e7cc968
Ted Szylowiec <tedszy@gmail.com>
2020-04-12 06:19:39 +0700

    fixed strings, removed parens, made integers

--- herbium.rkt
+++ herbium.rkt
@@ -1,27 +1,26 @@
 #s(publication
    ('nom-francais 'famille 'ref-victorin 'nom-latin 'indice 'comestibilite )
-   (#("Achillée millefeuille" "(COMPOSÉES)" "[592]"
+   (#("Achillée millefeuille" "COMPOSÉES" 592
                               "Achillea Millefolium" #f #f)
 
-    #("Actée blanche" "(RENONCULACÉES)" "[232]"
+    #("Actée blanche" "RENONCULACÉES" 232
                       "Actea pachypoda"
                       "Fruits blancs sur pédicelles épais (pachypoda) rouges.
                        Revers de la feuille moins poilu que A. rubra."
                       "toxique")
 
-    #("Actée rouge" "(RENONCULACÉES)" "[233]"
+    #("Actée rouge" "RENONCULACÉES" 233
                     "Actea rubra"
                     #f
                     "toxique")
     
-    #("Adiante du Canada" "(POLYPODIACÉES)" "[124]"
+    #("Adiante du Canada" "POLYPODIACÉES" 124
                           "Adiantum pedatum"
                           #f #f)
     
-    #("Agrostide blanche" "(GRAMINÉES)" "[793]"
+    #("Agrostide blanche" "GRAMINÉES" 793
                           "Agrostis stolonifera"
-                          "Taille : 50 cm. Inflorescence conique rougeâtre,teintée du blanc des étamines.
-                                 Épillets à une fleur. Branches courtes et très fleuries de l’inflorescence." #f)
+                          "Taille : 50 cm. Inflorescence conique rougeâtre,teintée du blanc des étamines. Épillets à une fleur. Branches courtes et très fleuries de l’inflorescence." #f)
 
 
 
