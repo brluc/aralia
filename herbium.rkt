69aafd6f518ca793b8ac03883445ee8d3f17296b
Luc Lamontagne <reliure@abbaye.ca>
2020-04-11 17:39:42 -0400

    debut base de donnee

---
+++ herbium.rkt
@@ -0,0 +1,79 @@
+#s(publication
+   ('nom-francais 'famille 'ref-victorin 'nom-latin 'indice 'comestibilite )
+   (#("Achillée millefeuille" "(COMPOSÉES)" "[592]"
+                              "Achillea Millefolium" #f #f)
+
+    #("Actée blanche" "(RENONCULACÉES)" "[232]"
+                      "Actea pachypoda"
+                      "Fruits blancs sur pédicelles épais (pachypoda) rouges.
+                       Revers de la feuille moins poilu que A. rubra." "toxique")
+
+    #("Actée rouge" "(RENONCULACÉES)" "[233]"
+                    "Actea rubra"
+                    #f "toxique")
+    
+    #("Adiante du Canada" "(POLYPODIACÉES)" "[124]"
+                          "Adiantum pedatum"
+                          #f #f)
+    
+    #("Agrostide blanche" "(GRAMINÉES)" "[793]"
+                          "Agrostis stolonifera"
+                                "Taille : 50 cm. Inflorescence conique rougeâtre,teintée du blanc des étamines.
+                                 Épillets à une fleur. Branches courtes et très fleuries de l’inflorescence." #f)
+
+
+
+
+
+
+
+
+    ))
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
