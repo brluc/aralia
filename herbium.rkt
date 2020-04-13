17043ac8ac06960237b2c1ac772ddc44dffe2e7d
Luc Lamontagne <reliure@abbaye.ca>
2020-04-12 20:27:56 -0400

    capsella pastoris

--- herbium.rkt
+++ herbium.rkt
@@ -1,128 +1,128 @@
 #s(herbium
    ('nom-francais 'famille 'ref-victorin 'nom-latin 'indice 'comestibilite )
-   (#("Achillée millefeuille" "COMPOSÉES" "[592]"
+   (#("Achillée millefeuille" "COMPOSÉES" "592"
                               "Achillea Millefolium" #f #f)
 
-    #("Actée blanche" "RENONCULACÉES" "[232]"
+    #("Actée blanche" "RENONCULACÉES" 232
                       "Actea pachypoda"
                       "Fruits blancs sur pédicelles épais (pachypoda) rouges.
                        Revers de la feuille moins poilu que A. rubra."
                       "toxique")
 
-    #("Actée rouge" "RENONCULACÉES" "[233]"
+    #("Actée rouge" "RENONCULACÉES" 233
                     "Actea rubra"
                     #f
                     "toxique")
     
-    #("Adiante du Canada" "POLYPODIACÉES" "[124]"
+    #("Adiante du Canada" "POLYPODIACÉES" 124
                           "Adiantum pedatum"
                           #f #f)
     
-    #("Agrostide blanche" "GRAMINÉES" "[793]"
+    #("Agrostide blanche" "GRAMINÉES" 793
                           "Agrostis stolonifera"
                           "Taille : 50 cm. Inflorescence conique rougeâtre,teintée du blanc des étamines. Épillets à une fleur. Branches courtes et très fleuries de l’inflorescence." #f)
 
-    #("Aigremoine striée" "ROSACÉES" "[344]"
+    #("Aigremoine striée" "ROSACÉES" 344
                           "Agrimonia striata" #f #f)
 
-    #("Ail des bois" "LILIACÉES" "[660]"
+    #("Ail des bois" "LILIACÉES" 660
                      "Allium tricoccum" #f "comestible")
 
-    #("Alpiste roseau" "GRAMINÉES" "[804]"
+    #("Alpiste roseau" "GRAMINÉES" 804
                        "Phalaris arundinacea" #f #f)
 
-    #("Amélanchier spp" "ROSACÉES" "[315]"
+    #("Amélanchier spp" "ROSACÉES" 315
                         "Amelanchier" #f "fruit comestible")
 
-    #("Anaphale marguerite" "COMPOSÉES" "[574]"
+    #("Anaphale marguerite" "COMPOSÉES" 574
                             "Anaphalis margaritacea" #f #f)
 
-    #("Ancolie du Canada" "RENONCULACÉES" "[228]"
+    #("Ancolie du Canada" "RENONCULACÉES" 228
                           "Aquilegia canadensis" "Fleurs écarlates à éperons droits." #f)
 
     #("Ancolie vulgaire" "RENONCULACÉES" #f
                          "Aquilegia vulgaris" "Fleurs bleues, pourpres ou blanches à éperons fortement recourbés." #f)
 
-    #("Antennaire" "COMPOSÉES" "[574]"
+    #("Antennaire" "COMPOSÉES" 574
                    "Antennaria howelii" #f #f)
 
 
-    #("Anthrisque des bois (persil sauvage)" "OMBELLIFÈRES" "[420]"
+    #("Anthrisque des bois (persil sauvage)" "OMBELLIFÈRES" 420
                                              "Anthriscus sylvestris" #f #f)
 
-    #("Apocyn à feuilles d’Androsème" "APOCYNACÉES" "[517]"
+    #("Apocyn à feuilles d’Androsème" "APOCYNACÉES" 517
                                       "Apocynum androsaemifolium" #f "toxique")
 
-    #("Aralie à grappes" "ARALIACÉES" "[412]"
+    #("Aralie à grappes" "ARALIACÉES" 412
                          "Aralia racemosa" #f "fruit comestible")
 
     #("Aralie à tige nue" "ARALIACÉES" #f
                           "Aralia nudicaulis" #f "fruit comestible")
 
-    #("Arisème petit-prêcheur" "ARACÉES" "[840]"
+    #("Arisème petit-prêcheur" "ARACÉES" 840
                                "Arisaema triphyllum (atrorubens)" #f "toxique")
 
-    #("Asaret du Canada" "ARISTOLOCHIACÉES" "[219]"
+    #("Asaret du Canada" "ARISTOLOCHIACÉES" 219
                          "Asarum canadense" #f "rhizome comestible")
 
-    #("Asclépiade commune" "ASCLÉPIADACÉES" "[519]"
+    #("Asclépiade commune" "ASCLÉPIADACÉES" 519
                            "Asclepias syriaca" #f #f)
 
-    #("Aster de la Nouvelle-Angleterre" "COMPOSÉES" "[608]"
+    #("Aster de la Nouvelle-Angleterre" "COMPOSÉES" 608
                                         "Aster (virgulus)  novae-angliae" #f #f)
 
-    #("Aster simple" "COMPOSÉES" "[609]"
+    #("Aster simple" "COMPOSÉES" 609
                      "Aster simplex Symphyotrichum lanceolatum" #f #f)
 
-    #("Athyrie fausse-thelyptère" "POLYPODIACÉES" "[132]"
+    #("Athyrie fausse-thelyptère" "POLYPODIACÉES" 132
                                   "Athyrium thelypteroides (-ridoides)" "Limbe doublement divisé. Sores allongés (bâtonnets)." #f)
 
-    #("Athyrie fougère-femelle" "POLYPODIACÉES" "[133]"
+    #("Athyrie fougère-femelle" "POLYPODIACÉES" 133
                                 "Athyrium Filix-femina" #f #f)
 
-    #("Aubépine spp" "ROSACÉES" "[296-314]"
+    #("Aubépine spp" "ROSACÉES" 296
                      "Crataegus" "Rameaux épineux." "fruit comestible")
 
-    #("Aulne crispé" "BÉTULACÉES" "[151]"
+    #("Aulne crispé" "BÉTULACÉES" 151
                      "Alnus crispa (viridis)" "Rare au Qc (vulnérable). Cocottes persistantes sur longues queues. Feuilles avec dents fines non doublement dentées." #f)
 
     #("Aulne rugueux" "BÉTULACÉES" #f
                       "Alnus rugosa (incana)" "Cocottes persistantes, sans queue. ne pas confondre avec A. viridis. Feuilles épaisses, plissées, vert foncé, rugueuses, pubescentes sur les deux faces, doublement dentées."
 
-    #("Barbarée vulgaire" "CRUCIFÈRES" "[269]"
+    #("Barbarée vulgaire" "CRUCIFÈRES" 269
                           "Barbarea vulgaris" "Floraison : mai-juillet."  #f)
 
-    #("Bardane majeure" "COMPOSÉES" "[570]"
+    #("Bardane majeure" "COMPOSÉES" 570
                         "Arctium Lappa" "Hauteur : 1-2 m. Gros capitules sur un long pédoncule. La queue des feuilles du bas est pleine, ne pas confondre avec A. minus." #f)
 
-    #("Bardane mineure" "COMPOSÉES"
+    #("Bardane mineure" "COMPOSÉES" #f
 			"Arctium minus" "Hauteur : 1-1,5 m. Feuilles teintées de blanc au-dessous. Petits capitules (1,5-3 cm)." #f)
 
-    #("Benoîte des ruisseaux" "ROSACÉES" "[344]"			
+    #("Benoîte des ruisseaux" "ROSACÉES" 344			
                               "Geum rivale" #f "rhizome tisane")
 
-    #("Benoîte du Canada" "ROSACÉES"
+    #("Benoîte du Canada" "ROSACÉES" #f
                           "Geum canadense" "Fleurs blanches, feuilles plus serrées, ne pas confondre avec G. aleppicum : fleurs jaunes." #f)
 
-    #("Benoîte d’Alep" "ROSACÉES" "[345]"
+    #("Benoîte d’Alep" "ROSACÉES" 345
                        "Geum aleppicum" #f #f)
 
-    #("Bermudienne commune" "IRIDACÉES" "[669]"
+    #("Bermudienne commune" "IRIDACÉES" 669
                             "Sisyrinchium montanum" #f #f)
 
-    #("Bident feuillu" "COMPOSÉES" "[566]"
+    #("Bident feuillu" "COMPOSÉES" 566
                        "Bidens frondosa" #f #f)
 
-    #("Bleuet fausse myrtille" "ÉRICACÉES" "[442]"
+    #("Bleuet fausse myrtille" "ÉRICACÉES" 442
                                "Vaccinium myrtilloides" "Rameaux nettement velus, feuilles poilues sur les deux faces." "fruit comestible")
 
     #("Bleuet (airelle) à feuille étroite" "ÉRICACÉES" #f
                                            "Vaccinium angustifolium" "Feuilles glabres." "fruit comestible")
 
-    #("Bouleau gris (à feuilles de peuplier)" "BÉTULACÉES" "[149]"
+    #("Bouleau gris (à feuilles de peuplier)" "BÉTULACÉES" 149
                                               "Betula populifolia" "Feuilles triangulaires longuement acuminées. Écorce ne s’exfoliant pas. Taches noires sous les branches." #f)
 
-    #("Bouleau blanc" "BÉTULACÉES" "[150]"
+    #("Bouleau blanc" "BÉTULACÉES" 150
                       "Betula papyrifera" #f "sève comestible")
     
     #("Bouleau jaune (merisier)" "BÉTULACÉES" #f
@@ -131,7 +131,8 @@
     #("Bouleau à feuilles cordées" "BÉTULACÉES" #f
                                    "Betula cordifolia ou Betula papyrifera var. cordifolia." #f #f)
 
-
+    #("Bourse à pasteur" "CRUCIFÈRES" 251
+                         "Capsella Bursu-pastoris" #f #f)
 
 
 
