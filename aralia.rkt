6b8e8022763a16dbf2840a596afe454cf1679d84
Ted Szylowiec <tedszy@gmail.com>
2020-04-28 00:23:14 +0700

    french local sort, groupified chapters IJK XYZ

--- aralia.rkt
+++ aralia.rkt
@@ -8,27 +8,85 @@
 (define (read-herbium)
   (call-with-input-file (herbium-database-filename) read))
 
-;; Herbium entries are grouped alphabetically according
-;; to the first character of name. The result is a list
-;; of cons pairs: the character and then the group of entries.
+;; Install French language pack in Ubuntu:
 ;;
-;; Name is the first element of each entry in herbium.
-;; It must be a string for this to work. Not a list of
-;; strings. So no alternate names.
-(define (group-by-name rows)
-  (let ((ht (make-hash)))
+;;    sudo apt-get install language-pack-fr
+;;
+;;    fr_BE.UTF-8... done
+;;    fr_CA.UTF-8... done  <= Canada
+;;    fr_CH.UTF-8... done
+;;    fr_FR.UTF-8... done  <= France
+;;    fr_LU.UTF-8... done
+;;
+;; Set current locale to French-France for now.
+;; Can set it to French-Canada if you like.
+
+(current-locale "fr_FR.UTF-8")
+
+;; Special character groups.
+;;
+;;         C 0067  I 0073  U 0085
+;; A 0065  Ç 0199  Î 0206  Ù 0217
+;; À 0192  E 0069  Ï 0207  Û 0219
+;; Â 0194  È 0200          Ü 0220
+;; Ä 0196  É 0201  O 0079
+;; Æ 0198  Ê 0202  Ô 0212
+;;         Ë 0203  Œ 0140
+
+(define A-group (list #\A #\À #\Â #\Ä #\Æ))
+(define C-group (list #\C #\Ç))
+(define E-group (list #\E #\È #\É #\Ê #\Ë))
+(define IJK-group (list #\I #\Î #\Ï #\J #\K))
+(define O-group (list #\O #\Ô #\Œ))
+(define U-group (list #\U #\Ù #\Û #\Ü))
+(define XYZ-group (list #\X #\Y #\Z))
+(define normal-group (list #\B #\D #\F #\G #\H #\L
+                           #\M #\N #\P #\Q #\R #\S
+                           #\T #\V #\W))
+
+;; Classify name under a chapter heading
+;; by its first character. Left-trim the name
+;; to be sure. Some chapters are grouped together,
+;; e.g., IJK.
+(define (get-chapter-key name)
+  (let ((c (string-ref
+            (string-locale-upcase
+             (string-trim name)) 0)))
+    (cond ((member c normal-group) (format "~a" c))
+          ((member c A-group) "A")
+          ((member c C-group) "C")
+          ((member c E-group) "E")
+          ((member c IJK-group) "I J K")
+          ((member c O-group) "O")
+          ((member c U-group) "U")
+          ((member c XYZ-group) "X Y Z")
+          (else
+           (error "get-chapter-key: failed on name: " name)))))
+
+(define (collect-into-chapters rows)
+  (let ((hash-table (make-hash)))
     (for-each
-     (lambda (row) 
-       (let ((ch (char-upcase (string-ref (car row) 0))))
-         (if (hash-has-key? ht ch)
-             (hash-update! ht ch (lambda (v)
-                                   (cons row v)))
-             (hash-set! ht ch (list row)))))
+     (lambda (row)
+       (match row
+         ((list name _ _ _ _ _)
+          (let ((chapter-key (get-chapter-key name)))
+            (if (hash-has-key? hash-table chapter-key)
+                (hash-update! hash-table
+                              chapter-key
+                              (lambda (v)
+                                (cons row v)))
+                (hash-set! hash-table
+                           chapter-key
+                           (list row)))))
+         (__ (error "collect-into-chapters: cannot match row: " row))))
      rows)
-    (sort (hash-map ht (lambda (k v)
-                         (cons k (sort v #:key car string<?))))
+    (sort (hash-map hash-table
+                    (lambda (k v)
+                      (cons k (sort v
+                                    #:key car
+                                    string-locale-ci<?))))
           #:key car
-          char<?)))
+          string-locale-ci<?)))
 
 (define (document body)
   (string-append
@@ -82,11 +140,13 @@
 
 ;; Group herbium entries alphabetically.
 ;; Organize them into alphabetic chapters.
+;; car of chapter is chapter name. cdr of chapter
+;; contains the plant entries.
 (define (make-body)
   (join-tex
-   (map (lambda (alphabetic-group)
+   (map (lambda (chapter)
           (string-append
-           (format "\\chapter*{~a}\n\n" (car alphabetic-group))
+           (format "\\chapter*{~a}\n\n" (car chapter))
            (join-tex
             (map (lambda (row)
                    (match (map false->blank row)
@@ -97,10 +157,9 @@
                      ((list name family ref
                             latin comment edibility)
                       (tex-herbe name latin family ref comment edibility))
-                     (_ (error "make-body: match herbium entry failed: "
-                               row))))
-                 (cdr alphabetic-group)))))
-        (group-by-name (read-herbium)))))
+                     (_ (error "make-body: cannot match row: " row))))
+                 (cdr chapter)))))
+        (collect-into-chapters (read-herbium)))))
 
 ;; Final product.
 (define (write-to-tex-file body)
@@ -111,4 +170,6 @@
        out))))
 
 (write-to-tex-file (make-body))
+
+
 
