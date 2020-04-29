2ba620dc37fbc9d98ae9da315953de397db688fa
Ted Szylowiec <tedszy@gmail.com>
2020-04-30 03:39:49 +0700

    diacritic elimination

--- fr-sort.rkt
+++ fr-sort.rkt
@@ -16,8 +16,8 @@
 ;;   Æ 0198 ==> AE
 ;;   Œ 0140 ==> OE
 
-(define (expand-ligatures my-string)
-  (let loop ((cs (string->list my-string))
+(define (expand-ligatures cs)
+  (let loop ((cs cs)
              (result empty))
     (cond ((null? cs)
            (reverse result))
@@ -32,25 +32,60 @@
                  (cons (car cs) result))))))
 
 (check-equal?
- (expand-ligatures "ŒÆÆŒ")
- '(#\O #\E #\A #\E #\A #\E #\O #\E)
+ (list->string (expand-ligatures (string->list "ŒÆÆŒ")))
+ "OEAEAEOE"
  "ligatures should be expanded properly")
 
-;; First do a regular diacritic-free comparison.
+;; Map out all the diacritic symbols into their
+;; plain base forms. 
+
+(define base-form-table
+  (hash #\À #\A #\Â #\A #\Ä #\A
+        #\Ç #\C
+        #\È #\E #\É #\E #\Ê #\E #\Ë #\E
+        #\Î #\I #\Ï #\I
+        #\Ô #\O
+        #\Ù #\U #\Û #\U #\Ü #\U))
+
+(define (eliminate-diacritics cs)
+  (map (lambda (c)
+         (if (hash-has-key? base-form-table c)
+             (hash-ref base-form-table c)
+             c))
+       cs))
+
+(check-equal?
+ (list->string
+  (eliminate-diacritics
+   (string->list "AÀÂÄCÇEÈÉÊËIÎÏOÔUÙÛÜ")))
+ "AAAACCEEEEEIIIOOUUUU"
+ "should eliminate diacritics and leave base forms")
+
+(check-equal?
+ (list->string
+  (eliminate-diacritics
+   (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ "should have no effect on base forms")
+
+;; First do a regular diacritic-free ligature-free comparison.
 ;; If the two strings are different, we are done.
 ;; If they are the same, go to next step: comparison
 ;; of diacritic characters.
 
+(define (base-form-sort cs)
+  (eliminate-diacritics
+   (expand-ligatures cs)))
 
 
 
-
-
-
-;; Secondary ordering of accents.
+;; Secondary ordering of diacritics:
 ;;
 ;;    vowel -- grave -- aigu -- circonflex -- trema
 ;;
+;;    C -- C cedille
+
+
 
 ;; "AÀÂÄ"
 ;; "CÇ"
